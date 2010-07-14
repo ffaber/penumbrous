8a4c4dc24ba8009737ec695a42a70dc90c91dbb7
ffaber <ffaber@309910c6-da4a-c14c-4845-526c06276573>
2010-07-14 15:42:42 +0000

    Initial commit.

---
+++ src/java/com/penumbrous/posts/p20100714/FirstPost.java
@@ -0,0 +1,11 @@
+// Copyright 2010 Google Inc.  All Rights Reserved 
+
+package com.penumbrous.posts.p20100714;
+
+class FirstPost {
+
+  public static void main(String[] args) {
+    System.err.println("");
+  }
+
+}
