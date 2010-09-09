7f8526dc0512b7ca9402dd1835a27a507c6984d1
ffaber <ffaber@309910c6-da4a-c14c-4845-526c06276573>
2010-09-09 20:26:04 +0000

    Adding GAE adwords code.

---
+++ trunk/src/java/com/penumbrous/adwordsapi/lib/AdWordsGaeHttpHandler.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2010 Fred Faber.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.penumbrous.adwordsapi.lib;
+
+import com.google.api.adwords.lib.ServiceAccountantManager;
+
+import com.penumbrous.axis.handlers.GaeHttpHandler;
+import org.apache.axis.AxisFault;
+import org.apache.axis.MessageContext;
+import org.apache.axis.handlers.BasicHandler;
+
+/**
+ *
+ * @author penumbrousdotcom@gmail.com (Fred Faber)
+ */
+public class AdWordsGaeHttpHandler extends BasicHandler {
+
+  private final GaeHttpHandler gaeHttpHandler;
+
+  public AdWordsGaeHttpHandler() {
+    this(new GaeHttpHandler());
+  }
+
+  AdWordsGaeHttpHandler(GaeHttpHandler gaeHttpHandler) {
+    this.gaeHttpHandler = gaeHttpHandler;
+  }
+
+  @Override
+  public void invoke(MessageContext msgContext) throws AxisFault {
+    gaeHttpHandler.invoke(msgContext);
+    ServiceAccountantManager.getInstance().recordMessage(msgContext);
+  }
+}
